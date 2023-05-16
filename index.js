ad390d57f5cd4615a41e4230d25903f83bfb3df1
scattamooch <71458730+scattamooch@users.noreply.github.com>
2023-05-16 10:19:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+// Save game character as a variable
+const dodger = document.getElementById("dodger");
+
+// Change the color of the character
+dodger.style.backgroundColor = "#FF69B4";
+
+//Keystroke logger
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+//Dodger can move 
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });  
+
+//Dodger can move right
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const containerWidth = dodger.parentElement.clientWidth;
+    const dodgerWidth = dodger.clientWidth;
+    const maxRight = containerWidth - dodgerWidth;
+
+    if (left < maxRight) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key ==="ArrowRight") {
+        moveDodgerRight();
+    }
+});
